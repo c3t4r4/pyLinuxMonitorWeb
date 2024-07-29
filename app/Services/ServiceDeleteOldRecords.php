a3ee604b24cb6196dfc3aa5444bd33929f46ba2e
c3t4r4 <neocetara@hotmail.com>
2024-07-29 08:04:25 -0300

    Update Delete

--- app/Services/ServiceDeleteOldRecords.php
+++ app/Services/ServiceDeleteOldRecords.php
@@ -4,16 +4,27 @@ namespace App\Services;
 
 use App\Models\Disk;
 use App\Models\Log;
+use App\Models\Node;
 use Carbon\Carbon;
 
 class ServiceDeleteOldRecords
 {
     public static function Remove(): void
     {
+        $nodes = Node::with('lastlog')->get();
+
+        $logsID = [];
+        $disksID = [];
+
+        foreach ($nodes as $node){
+            $logsID[] = $node->lastlog->id;
+            $disksID[] = $node->lastlog->disk_id;
+        }
+
         $anterior = Carbon::now()->subMinutes(3);
 
-        Log::where('created_at', '<', $anterior)->delete();
-        Disk::where('created_at', '<', $anterior)->delete();
+        Log::where('created_at', '<', $anterior)->whereNotIn('id',$logsID)->delete();
+        Disk::where('created_at', '<', $anterior)->whereNotIn('id',$disksID)->delete();
 
     }
 }
